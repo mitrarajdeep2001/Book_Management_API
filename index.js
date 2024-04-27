8b99c79a4d17df086e1a7946c73174fbcb386d11
Rajdeep Mitra <rajdeepmitra8@gmail.com>
2024-04-27 12:28:27 +0530

    made changes

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-const express = require('express');
-const connectDB = require('./Database/config'); // Import the connectDB function
-const authRoutes = require('./Routes/auth');
-const bookRoutes = require('./Routes/book');
+const express = require("express");
+const connectDB = require("./Database/config"); // Import the connectDB function
+const authRoutes = require("./Routes/auth");
+const bookRoutes = require("./Routes/book");
 
 async function main() {
-    const app = express();
-    const PORT = process.env.PORT || 3000;
+  const app = express();
+  const PORT = process.env.PORT || 3000;
 
-    // Middleware
-    app.use(express.json());
+  // Middleware
+  app.use(express.json());
 
-    try {
-        // MongoDB connection
-        await connectDB();
-        console.log('MongoDB connected');
+  try {
+    // MongoDB connection
+    await connectDB();
+    console.log("MongoDB connected");
 
-        // Routes
-        app.use('/auth', authRoutes);
-        app.use('/book', bookRoutes);
+    // Routes
+    app.use("/", async (req, res) => {
+      res.send("Welcome! to my book management API.");
+    });
+    app.use("/auth", authRoutes);
+    app.use("/book", bookRoutes);
 
-        // Start server
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
-        });
-    } catch (err) {
-        console.error('Error starting server:', err);
-    }
+    // Start server
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Error starting server:", err);
+  }
 }
 
 // Call the main function to start the server
